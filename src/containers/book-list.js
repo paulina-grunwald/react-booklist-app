c389cde1226694c7fadb87332610d849023a6685
Paulina Grunwald <paulina.grunwald@quasset.com>
2018-10-26 14:49:36 +0200

    add container that will contain details of selected book

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux'
-
+import { selectBook } from '../actions/actionCreator_selectBook'
+import { bindActionCreators } from 'redux'
 class BookList extends Component {
 
   renderList() {
@@ -8,15 +9,14 @@ class BookList extends Component {
       return (
         <li
         key={book.title}
-       
+        onClick={() => this.props.selectBook(book)}
         className="list-group-item"
       >
-          {book.title}
-        </li>
+        {book.title}
+      </li>
       )
     })
   }
-
   render() {
     return (
       <ul className="list-group col-sm-4">
@@ -27,16 +27,22 @@ class BookList extends Component {
 }
 
 function mapStateToProps(state) {
-
   return {
   // What is returned from this function will be available 
   // as a prop inside BookList component 
   // Specify exactly which slice of the state we want to provide to our component
   books: state.books
-
   }
+}
+
+//Anything returned from this function will be availavle as props
+// in the BookList Container
+function mapDispatchToProps(dispatch) {
+  // Whenever selectBook is called the result
+  // should be passed to all reducers
+  return bindActionCreators({selectBook: selectBook},dispatch)
 }
 
 // connect() function component to be connected to the store
 
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps,mapDispatchToProps)(BookList);
